86eeec925283030f7f5a1419e22db7f3462a81db
Felipe Baz <54612024+Felipe-Baz@users.noreply.github.com>
2023-08-28 17:03:46 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -7,18 +7,6 @@ from api.routers import sentiment
 # Core Application Instance
 app = FastAPI()
 
-origins = [
-    "http://localhost:8000",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 
 @app.on_event("startup")
 async def startup_event():
